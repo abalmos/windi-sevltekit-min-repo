d9d6c220581c7a9747c004043be3b2586d8cbca2
Andrew Balmos <andrew@balmos.org>
2021-09-06 10:35:58 -0400

    Add lang="postcss" and nested css
    Signed-off-by: Andrew Balmos <andrew@balmos.org>

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,16 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<style>
+<style lang="postcss">
 	h1 {
 		@apply text-purple-600 font-light;
 	}
+
+  p {
+    @apply text-pink-400 font-bold;
+
+    a {
+        @apply text-black-500;
+    }
+  }
 </style>
